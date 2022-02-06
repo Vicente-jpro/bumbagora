eb93773808be1e90c8968398c248d7f43a99b618
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-02-06 21:30:01 +0100

    Added user details to user devise

--- db/migrate/20220204211430_devise_create_users.rb
+++ db/migrate/20220204211430_devise_create_users.rb
@@ -3,6 +3,12 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+
+      ## User details
+      t.string :username
+      t.string  :name_country
+      t.string :localization_description
+      
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
