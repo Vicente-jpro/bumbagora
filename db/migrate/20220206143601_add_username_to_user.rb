7f5a37c6403e2bea67aa23c500391ab0bc5b0ff0
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-02-06 21:26:48 +0100

    Removed username, name_country and localization_description

--- db/migrate/20220206143601_add_username_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddUsernameToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :username, :string
-    add_column :users, :name_country, :string
-    add_column :users, :localization_description
-  end
-end
