dbf4e146d92a95b8246ae3dbd53e77e8b3cd06ee
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-03-27 14:09:43 +0100

    db seed added

--- db/migrate/20220205005448_remove_type_from_job.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTypeFromJob < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :jobs, :type, :string
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+1.times do 
+   user = User.new({
+        username: "Vicente",
+        name_country: "Angola",
+        localization_description: "Luanda/Kilamba", 
+        email: "vicente@gmail.com",
+        password: "vicente",
+        password_confirmation: "vicente"
+        })
+   user.save
+end
+
+Category.create(name: "Programming")
+Category.create(name: "Construction")
+Category.create(name: "Movie")
+Category.create(name: "Authors")
+
+3.times do |job_number|
+
+    Job.create(
+        title: "Ruby on rails Dev #{job_number}", 
+        description: "Developer new feacture", 
+        salary: 4500*job_number, 
+        category_id: job_number
+    )
+end
