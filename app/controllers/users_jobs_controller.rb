cf003f905db002231c3506f83500708961b530eb
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-04-09 14:45:36 +0100

    User subscription done

--- app/controllers/users_jobs_controller.rb
+++ app/controllers/users_jobs_controller.rb
@@ -11,9 +11,13 @@ class UsersJobsController < ApplicationController
    # POST /users_jobs/:id/apply
    def apply 
     save_job_into_user_creater(@job)
-    
-  debugger
+    @jobs ||= Job.all
+    flash[:notice] = "Subscrição feita com sucesso para vaga #{@job.title}"
+    render "jobs/index", job: @jobs 
+   
    end
+
+   
 
    private 
     def set_job 
