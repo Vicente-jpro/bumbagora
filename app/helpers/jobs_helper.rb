656ab6d77894d38db63fb60c2b31ab4166b39ce9
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-04-20 21:40:05 +0100

    count time to destroy done

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,4 +2,21 @@ module JobsHelper
     def is_the_owner_of_the_job?(job)
       (user_signed_in?) and (job.user_id == current_user.id)
     end
+
+     def time_ago_in_days(job)
+       if time_array_lenght(job)
+         time_ago_in_words(job.created_at).split.first > 20
+         return true
+       end
+       return false
+     end 
+    
+
+     def time_array_lenght(job)
+       time_ago_in_words(job.created_at).split.length == 2
+     end
+
+     def day_is_thirty(job)
+       time_ago_in_words(job.created_at) == "30 days"
+     end
 end
