5198a7166884520370c148a2ada81aef117e57df
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-04-21 00:28:27 +0100

    image attach added to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
- 
+  
+  has_one_attached :image
   has_many :users_jobs
   has_many :jobs, through: :users_jobs, dependent: :destroy
   
