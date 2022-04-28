78e1a50ae8bc4c6922cfb8092e925d9f4d0449da
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-04-28 03:48:55 +0100

    countries added

--- db/seeds.rb
+++ db/seeds.rb
@@ -195,116 +195,116 @@ end
 "Mali"
 "Malta"
 "Marshall Islands"
-Martinique
-Mauritania
-Mauritius
-Mayotte
-Mexico
-Micronesia, Federated States of
-Moldova
-Monaco
-Mongolia
-Montenegro
-Montserrat
-Morocco
-Mozambique
-Myanmar
-Namibia
-Nauru
-Nepal
-Netherlands
-New Caledonia
-New Zealand
-Nicaragua
-Niger
-Nigeria
-Niue
-Norfolk Island
-North Korea
-North Macedonia
-Northern Mariana Islands
-Norway
-Oman
-Pakistan
-Palau
-Palestine, State of
-Panama
-Papua New Guinea
-Paraguay
-Peru
-Philippines
-Pitcairn
-Poland
-Portugal
-Puerto Rico
-Qatar
-Romania
-Russian Federation
-Rwanda
-Réunion
-Saint Barthélemy
-Saint Helena, Ascension and Tristan da Cunha
-Saint Kitts and Nevis
-Saint Lucia
-Saint Martin (French part)
-Saint Pierre and Miquelon
-Saint Vincent and the Grenadines
-Samoa
-San Marino
-Sao Tome and Principe
-Saudi Arabia
-Senegal
-Serbia
-Seychelles
-Sierra Leone
-Singapore
-Sint Maarten (Dutch part)
-Slovakia
-Slovenia
-Solomon Islands
-Somalia
-South Africa
-South Georgia and the South Sandwich Islands
-South Korea
-South Sudan
-Spain
-Sri Lanka
-Sudan
-Suriname
-Svalbard and Jan Mayen
-Sweden
-Switzerland
-Syrian Arab Republic
-Taiwan
-Tajikistan
-Tanzania
-Thailand
-Timor-Leste
-Togo
-Tokelau
-Tonga
-Trinidad and Tobago
-Tunisia
-Turkey
-Turkmenistan
-Turks and Caicos Islands
-Tuvalu
-Uganda
-Ukraine
-United Arab Emirates
-United Kingdom
-United States
-United States Minor Outlying Islands
-Uruguay
-Uzbekistan
-Vanuatu
-Venezuela
-Vietnam
-Virgin Islands, British
-Virgin Islands, U.S
-Wallis and Futuna
-Western Sahara
-Yemen
-Zambia
-Zimbabwe
-Åland Islands
+"Martinique"
+"Mauritania"
+"Mauritius"
+"Mayotte"
+"Mexico"
+"Micronesia, Federated States of"
+"Moldova"
+"Monaco"
+"Mongolia"
+"Montenegro"
+"Montserrat"
+"Morocco"
+"Mozambique"
+"Myanmar"
+"Namibia"
+"Nauru"
+"Nepal"
+"Netherlands"
+"New Caledonia"
+"New Zealand"
+"Nicaragua"
+"Niger"
+"Nigeria"
+"Niue"
+"Norfolk Island"
+"North Korea"
+"North Macedonia"
+"Northern Mariana Islands"
+"Norway"
+"Oman"
+"Pakistan"
+"Palau"
+"Palestine, State of"
+"Panama"
+"Papua New Guinea"
+"Paraguay"
+"Peru"
+"Philippines"
+"Pitcairn"
+"Poland"
+"Portugal"
+"Puerto Rico"
+"Qatar"
+"Romania"
+"Russian Federation"
+"Rwanda"
+"Réunion"
+"Saint Barthélemy"
+"Saint Helena, Ascension and Tristan da Cunha"
+"Saint Kitts and Nevis"
+"Saint Lucia"
+"Saint Martin (French part)"
+"Saint Pierre and Miquelon"
+"Saint Vincent and the Grenadines"
+"Samoa"
+"San Marino"
+"Sao Tome and Principe"
+"Saudi Arabia"
+"Senegal"
+"Serbia"
+"Seychelles"
+"Sierra Leone"
+"Singapore"
+"Sint Maarten (Dutch part)"
+"Slovakia"
+"Slovenia"
+"Solomon Islands"
+"Somalia"
+"South Africa"
+"South Georgia and the South Sandwich Islands"
+"South Korea"
+"South Sudan"
+"Spain"
+"Sri Lanka"
+"Sudan"
+"Suriname"
+"Svalbard and Jan Mayen"
+"Sweden"
+"Switzerland"
+"Syrian Arab Republic"
+"Taiwan"
+"Tajikistan"
+"Tanzania"
+"Thailand"
+"Timor-Leste"
+"Togo"
+"Tokelau"
+"Tonga"
+"Trinidad and Tobago"
+"Tunisia"
+"Turkey"
+"Turkmenistan"
+"Turks and Caicos Islands"
+"Tuvalu"
+"Uganda"
+"Ukraine"
+"United Arab Emirates"
+"United Kingdom"
+"United States"
+"United States Minor Outlying Islands"
+"Uruguay"
+"Uzbekistan"
+"Vanuatu"
+"Venezuela"
+"Vietnam"
+"Virgin Islands, British"
+"Virgin Islands, U.S"
+"Wallis and Futuna"
+"Western Sahara"
+"Yemen"
+"Zambia"
+"Zimbabwe"
+"Åland Islands"
