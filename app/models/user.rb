c73a5062021aea3da6f354b74bf31e7b92c30200
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-05-28 17:04:02 +0100

    USer validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
   has_many :jobs, through: :users_jobs, dependent: :destroy
   belongs_to :country
 
+  validates_presence_of :country
+  
   enum type_subscription: {Candidate: "Candidate", Company: "Company"}
 
   scope :find_by_id, ->(user) { where(id: user.id)}
