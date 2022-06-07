0dd6f56a512c9128699715b87ec8d1ff7ee6bb8f
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-06-07 06:27:50 +0100

    Candidate validation added

--- db/migrate/20220501144329_add_country_id_to_user.rb
+++ db/migrate/20220501144329_add_country_id_to_user.rb
@@ -1,5 +1,5 @@
 class AddCountryIdToUser < ActiveRecord::Migration[7.0]
   def change
-    add_reference :users, :country, null: false, foreign_key: true
+    add_reference :users, :country, null: false, foreign_key: true, default: 1
   end
 end
