9be78cb5bd1fae3583e3afb02461a05b44b9be5c
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-06-14 09:59:42 +0100

    Controller registration modified

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,18 +3,40 @@ class RegistrationsController < Devise::RegistrationsController
   	super
   end
 
+    # PATCH/PUT /users/1 or /users/1.json
   def update
-  	super
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to jobs_url, notice: "user was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
- def create
- 	debugger
- end
+# POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to jobs_url, notice: "user was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   private
+   def user_params
+    @user = User.find(params[:id])
+   end
 
 	 def user_params
-		params.require(:job).permit(:username, :type_subscription, :country_id, :localization_description, :image, :document)
+		params.require(:user).permit(:username, :type_subscription, :country_id, :localization_description, :image, :document)
 	 end
 
 end
