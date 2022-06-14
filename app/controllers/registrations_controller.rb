eb55645b9430bd6632e65991a9fa3b576c35398a
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-06-14 15:32:34 +0100

    Message confirmed email added

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,23 +5,26 @@ class RegistrationsController < Devise::RegistrationsController
 
     # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to jobs_url, notice: "user was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to jobs_url, notice: "Usuários actualizado com sucesso." }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
-    end
+     
   end
 
 # POST /users or /users.json
   def create
+
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
-        format.html { redirect_to jobs_url, notice: "user was successfully created." }
+        format.html { redirect_to jobs_url, notice: "Você receberá um e-mail com instruções sobre como confirmar seu endereço de e-mail em alguns minutos." }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
