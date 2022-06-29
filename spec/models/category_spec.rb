a056acfb02bb7b2641b36f2c78331385aa816a80
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-06-29 16:50:43 +0100

    File new passaword addited

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   
-  describe "Category model" do 
+  describe "Category" do 
     let(:category) { create(:category) }
 
     it "has a name" do 
