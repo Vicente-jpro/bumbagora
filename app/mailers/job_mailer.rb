60390e2208bd15775575e0f6b32170abdf7ef74e
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-08-08 21:13:18 +0100

    Job class added

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -1,13 +1,17 @@
 class JobMailer < ApplicationMailer
-	default from: 'vicente.simao@bumbagora.com'
+  default from: 'vicente.simao@bumbagora.com'
 
-	def invitation
-	 @candidate = params[:user_candidate]
-	 @job = params[:job]
-	 @company = params[:user_company]
+  def invitation
+   @candidate = params[:user_candidate]
+   @job = params[:job]
+   @company = params[:user_company]
+   @category_id = params[:category_id]
 
-	 @url  = 'www.bumbagora.com'
-	 message = "A empresa #{company.username} convidou-te para fazer parte de uma entrevista na vaga de #{@job.title}. Boa sorte."
-	 mail(to: @candidate.email, subject: "Olá #{@candidate.username}! #{message}" )
-	end
+   @url = "http://127.0.0.1:3000/jobs/search_with_category?category_id=#{@category_id}"
+   #@url  = 'www.bumbagora.com'
+   message = "A empresa #{company.username} convidou-te para fazer parte de uma entrevista na vaga de #{@job.title}. Boa sorte. Clica neste link #{@url}"
+   mail(
+    to: @candidate.email, 
+    subject: "Olá #{@candidate.username}! #{message}" )
+  end
 end
