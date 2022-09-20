2910a40241bd5a04c5ec4bea53b45a324613def2
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-09-20 11:50:21 +0100

    Exist candidate removed

--- app/helpers/users_jobs_helper.rb
+++ app/helpers/users_jobs_helper.rb
@@ -3,8 +3,8 @@ module UsersJobsHelper
         user_signed_in? and  job.users.find_by_id(current_user).any?
     end
 
-    def exit_candidates?(job)
-       candidates = UsersJob.find_users_registed_to_this_job(job).first
-       return !candidates.nil?
-    end
+    # def exit_candidates?(job)
+    #    candidates = UsersJob.find_users_registed_to_this_job(job).first
+    #    return !candidates.nil?
+    # end
 end
