f395cd2f10b98ab8e5ca054c291c07a9a5abb2e4
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-09-26 00:43:58 +0100

    Some changes done

--- spec/requests/jobs_controller_test_spec.rb
+++ spec/requests/jobs_controller_test_spec.rb
@@ -1,3 +1,21 @@
+require 'rails_helper'
+
 class JobsControllerTest < ActionController::TestCase
 	include Devise::Test::IntegrationHelpers # Rails >= 5
+
+  describe "SEARCH /jobs/search" do 
+    let(:job) {create(:job)}
+    let(:user) {create(:user)}
+
+
+    it "should have a title" do   
+      get "/jobs/search?job=#{job.title}"
+
+      @request.session[:user_id] = job.user_id
+
+      expect(response).to have_http_status(200)
+      expect(assigns(:jobs)).to render_template(:search)
+    end
+  end
+
 end
