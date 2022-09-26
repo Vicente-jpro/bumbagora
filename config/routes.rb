5a94563bb5deb9d68ff52020e602987d9290ec7a
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-09-26 20:51:19 +0100

    tables added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :rooms
   
   mount Sidekiq::Web => '/sidekiq'
 
---
+++ spec/factories/messages.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :message do
+    room { nil }
+    content { "MyText" }
+  end
+end
