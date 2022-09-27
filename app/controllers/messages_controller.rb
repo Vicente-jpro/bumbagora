7fda6a92f2c00db66677ec2409fbbf996a40c08a
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-09-28 00:10:15 +0100

    Chat comunication done

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,6 @@ class MessagesController < ApplicationController
    @message = @room.messages.create!(message_params)
    
    respond_to do |format|
-   	 format.turbo_stream
    	 format.html { redirect_to @room }
    end
  end
