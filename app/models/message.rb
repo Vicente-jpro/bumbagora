eab161873288b201ab4dd5d2495894ec5ab6f6e4
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-09-28 01:03:22 +0100

    Chat application done

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :room
-  broadcasts_to :room
+  after_create_commit -> { broadcast_append_to room }
+  after_destroy_commit -> { broadcast_remove_to room }
 end
