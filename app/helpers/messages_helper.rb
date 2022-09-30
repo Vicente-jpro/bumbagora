0e7f2852d114ded722d84ae6c156e60bf06aeda4
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-09-30 21:57:29 +0100

    css chat contet modified

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,6 +1,17 @@
 module MessagesHelper
+
   def message_color(message)
-  	return "teal-lighten-5" if message.user.Admin? or message.user.Empresa?
+  	return "teal-lighten-5" if user_is_admin_or_company?(message)
   	"indigo-lighten-5"
   end
+
+  def text_direction(message)
+    return "justify-content-start"  if user_is_admin_or_company?(message)
+    "justify-content-end"
+  end
+
+  private
+    def user_is_admin_or_company?(message)
+      (message.user.Admin? or message.user.Empresa?)
+    end
 end
