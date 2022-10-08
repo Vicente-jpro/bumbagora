64e443a79d0408d30e697c41ab0d31cbc5324dd6
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-10-08 10:43:52 +0100

    Error removed

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -64,7 +64,7 @@ class RoomsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_room
-      @room = Room.find(params[:id])
+      @room = Room.find_by(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
