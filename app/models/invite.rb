33a454dec5031bf237c3fb4d71b3901c7f2850a4
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-10-08 15:36:30 +0100

    Send message

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -5,4 +5,19 @@ class Invite < ApplicationRecord
   def self.find_by_room(room) 
     find_by(room_id: room.id)
   end
+
+  def self.find_company_room(room)
+    company = find_by(room_id: room.id)
+    company.room
+  end
+
+  def self.find_company_messages_with_room(room)
+     room = find_company_room(room)
+     messages = Message.find_sent(room.user)
+     messages
+  end
+
+  def self.find_received_candidate(user)
+    where(user_id: user.id, open: false)
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,16 @@ class Message < ApplicationRecord
   belongs_to :user
   after_create_commit -> { broadcast_append_to room }
   after_destroy_commit -> { broadcast_remove_to room }
+
+  def self.find_received_candidate(user)
+    where(user_id: user.id, open: false)
+  end
+
+  def self.find_sent(user)
+    where(user_id: user.id, open: false)
+  end
+  def self.find_received_company(room)
+    where(user_id: room.user_id, open: false)
+  end
+
 end
