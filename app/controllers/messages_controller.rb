7b085bff6ede33db3d72fc3ae6d4f5e522166edf
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-10-12 00:58:32 +0100

    Answer message feature done

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
  before_action :set_room, only: %i[ new create]
 
+
  def new
   @message = @room.messages.new 
  end
@@ -8,17 +9,46 @@ class MessagesController < ApplicationController
  def create
    @message = @room.messages.create!(message_params)
 
+   open_messages(@message)
+
    respond_to do |format|
      format.turbo_stream
    	 format.html { redirect_to @room }
    end
  end
 
- def set_room 
-   @room = Room.find(params[:room_id])
- end
+ private
+   def set_room 
+     @room = Room.find(params[:room_id])
+   end
 
- def message_params
-   params.require(:message).permit(:content, :user_id)
- end
+   def message_params
+     params.require(:message).permit(:content, :user_id)
+   end
+
+   def open_messages(message)
+     user_id = 0
+
+     # get company_id to answer messages
+     if current_user.Candidato?
+      user_id = message.room.user.id
+     else
+     # get candidate_id to answer messages
+      invite = Invite.find_by_room(@message.room)
+      user_id = invite.user.id
+     end
+       
+     # answer all last messages
+     Message.where(
+              #Company_id
+              user_id: user_id, 
+              room_id: message.room.id,
+              open: false, 
+            )
+            .find_each do |m| 
+                m.update!(open: true) 
+             end
+
+   end
+
 end
--- db/migrate/20221010211553_fix_column_name.rb
+++ db/migrate/20221010211553_fix_column_name.rb
@@ -1,6 +1,7 @@
 class FixColumnName < ActiveRecord::Migration[7.0]
   def self.up
-    rename_column :invites, :open, :opened
+    rename_column :invites, :opened, :opened, default: false
+    rename_column :messages, :openned, :boolean, default: false
   end
 
   def self.down
