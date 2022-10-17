3442afedb71e85ea31998db36032dfa8dd9df420
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-10-17 21:56:55 +0100

    method find_rooms_by_user added

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,4 +4,6 @@ class Room < ApplicationRecord
   belongs_to :user
   has_many :messages, dependent: :destroy
   has_one :invite, dependent: :destroy
+
+  scope :find_rooms_by_user, ->(room) { where(user_id: room.user_id)}
 end
