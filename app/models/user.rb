b99299a8c1c60c2d51f100e11aabe8d2bcc1a3ad
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-06-22 06:43:59 +0100

    Local storeg added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable #, :validatable, :confirmable
 
   has_one_attached :image, dependent: :destroy do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
