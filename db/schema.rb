62e93a9c334a12c00629a1350302c041586dc36b
vicente-jpro <vicenteviciii@outlook.com>
2025-04-19 14:43:07 +0100

    download cv added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_18_222221) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_19_124255) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -69,6 +69,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_18_222221) do
     t.text "description"
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_experiences_on_user_id"
+  end
+
+  create_table "fileks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "invites", force: :cascade do |t|
