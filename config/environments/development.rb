c1601bb60d820e51f36b1e3c28cefbd35ab8b631
vicente-jpro <vicenteviciii@outlook.com>
2025-06-07 19:07:08 +0100

    feat: app file added.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,21 +3,20 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
 
 
-config.action_mailer.default_url_options = {host: "localhost:3000"}
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
-#config.action_mailer.smtp_settings = {
- # :user_name => "vicente.simao@bumbagora.com",
- # :password => "c65e2b48dd6a2043e2c33f333a2f031a",
- # :domain => "localhost:3000",
- # :address => 'smtp.ionos.com',
- # :port => 587,
- # :authentication => :plain,
- # :enable_starttls_auto => true
+  config.action_mailer.default_url_options = {host: "localhost:3000"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+
+  #config.action_mailer.smtp_settings = {
+#  :user_name => "vicente.simao@bumbagora.com",
+#  :password => "c65e2b48dd6a2043e2c33f333a2f031a",
+#  :domain => "localhost:3000",
+#  :address => 'smtp.ionos.com',
+#  :port => 587,
+#  :authentication => :plain,
+#  :enable_starttls_auto => true
 #}
-
-
 
 
   # Settings specified here will take precedence over those in config/application.rb.
