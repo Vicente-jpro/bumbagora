8bdee6a35326366c62af251489b3c5a654947541
vicente-jpro <vicenteviciii@outlook.com>
2025-06-10 08:53:08 +0100

    feat: version changed

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,9 +1,12 @@
 require 'resque/server'
 
 if Rails.env.development?
-    Resque.redis = Redis.new(host: '127.0.0.1', port: '6379')
+  Resque.redis = Redis.new(host: '127.0.0.1', port: '6379')
+elsif ENV['REDIS_URL']
+  uri = URI.parse(ENV['REDIS_URL'])
+  REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+  Resque.redis = REDIS
 else
-   uri = URI.parse(ENV['REDIS_URL'])
-   REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-   Resque.redis = REDIS
+  warn "[WARNING] REDIS_URL not set. Resque not configured."
 end
+
