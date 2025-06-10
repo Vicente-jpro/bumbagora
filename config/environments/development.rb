3dc26057a03d066414b3f27aa1cf6cbf39652dd4
vicente-jpro <vicenteviciii@outlook.com>
2025-06-10 09:19:41 +0100

    feat: Development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
 
 
+  config.hosts << Rails.application.credentials.google_cloud_app_url
   config.action_mailer.default_url_options = {host: "localhost:3000"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
